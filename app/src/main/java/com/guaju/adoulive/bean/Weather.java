14545d93522de32263da3d0d94daa6c934a3e6ee
guaju <guaju@126.com>
2018-01-10 15:11:37 +0800

    httputil

---
+++ app/src/main/java/com/guaju/adoulive/bean/Weather.java
@@ -0,0 +1,8 @@
+package com.guaju.adoulive.bean;
+
+/**
+ * Created by guaju on 2018/1/10.
+ */
+
+public class weather {
+}
---
+++ app/src/main/java/com/guaju/adoulive/httputil/OkHttpHelper.java
@@ -0,0 +1,8 @@
+package com.guaju.adoulive.httputil;
+
+/**
+ * Created by guaju on 2018/1/10.
+ */
+
+public class OkHttpHelper {
+}
