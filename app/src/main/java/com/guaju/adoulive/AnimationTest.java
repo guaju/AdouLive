74901b96917e07c521b307fd5b294437819f59fd
guaju <guaju@126.com>
2018-01-22 15:19:56 +0800

    动画

--- app/src/main/java/com/guaju/adoulive/AnimationTest.java
+++ app/src/main/java/com/guaju/adoulive/AnimationTest.java
@@ -2,12 +2,15 @@ package com.guaju.adoulive;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.guaju.adoulive.widget.danmu.DanmuItemView;
 import com.guaju.adoulive.widget.gift.GiftItem;
-import com.guaju.adoulive.widget.gift.GiftSendDialog;
 
 /**
  * Created by guaju on 2018/1/16.
@@ -15,19 +18,114 @@ import com.guaju.adoulive.widget.gift.GiftSendDialog;
 
 public class AnimationTest extends Activity{
 
+
+    private static final int FIRST_GIFT_SEND_FLAG = -1;
+    public static final int REPEAT_GIFT_SEND_FLAG =1 ;
+    //倒计时时间范围
+    private int repeatTimeLimit=10;
+
+    long firstSendTimeMillion;
+    Handler repeatGiftTimer=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case FIRST_GIFT_SEND_FLAG:
+                    if (repeatTimeLimit>0){
+                        repeatTimeLimit--;//开始倒数
+                        sendEmptyMessageDelayed(FIRST_GIFT_SEND_FLAG,80);
+                        bt.setText("发送（"+repeatTimeLimit+")");
+                        //用户现在可以连发
+                    }else{
+                        //倒计时已经数完了，可以重新再开始
+                        giftItem.setIsRepeat(false);
+                        firstSendTimeMillion=0;
+                        repeatTimeLimit=10;
+                        bt.setText("发送");
+                        //用户不能再连发了
+                    }
+
+                    break;
+
+                case REPEAT_GIFT_SEND_FLAG:
+                     //停止第一个事件 的处理
+
+
+                    if (repeatTimeLimit>0){
+                        repeatTimeLimit--;//开始倒数
+                        sendEmptyMessageDelayed(REPEAT_GIFT_SEND_FLAG,80);
+                        bt.setText("发送（"+repeatTimeLimit+")");
+
+                        //用户现在可以连发
+                    }else{
+                        //倒计时已经数完了，可以重新再开始
+                        giftItem.setIsRepeat(false);
+                        giftItem.repeatSendWithoutAddNum();
+                        firstSendTimeMillion=0;     
+                        repeatTimeLimit=10;
+                        bt.setText("发送");
+                        //用户不能再连发了
+                    }
+
+
+                    break;
+                default:
+                    super.handleMessage(msg);
+                    break;
+            }
+
+
+
+        }
+    };
+
     private DanmuItemView dd;
     private GiftItem giftItem;
     private ImageView imageView;
+    private Button bt;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
-//        giftItem = findViewById(R.id.gift);
-//        imageView = findViewById(R.id.iv_gift_icon);
-//        giftItem.startAnimate();
-////        giftItem.startAnimate();
-        GiftSendDialog giftSendDialog = new GiftSendDialog(this,R.style.custom_dialog);
-        giftSendDialog.show();
+        giftItem = findViewById(R.id.gift);
+        bt = findViewById(R.id.bt);
+        bt.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                //先清空动画
+//                giftItem.clearAnimation();
+
+
+                //在第一次点的时候开始计时
+                if (firstSendTimeMillion==0){
+                    //第一次点击不是连发，设置给giftitem
+                    giftItem.setIsRepeat(false);
+                    //拿到第一次点的时间
+                    firstSendTimeMillion=System.currentTimeMillis();
+                    repeatGiftTimer.sendEmptyMessage(FIRST_GIFT_SEND_FLAG);
+                    //再执行动画
+                    giftItem.startAnimate();
+
+                }
+                else{//如果属于连击的话,需要把倒计时再从10开始倒数，并且增加礼物数
+                    //属于连发
+                    giftItem.setIsRepeat(true);
+                    giftItem.repeatSend();//连发操作
+
+                    //清空两个handler的处理
+                    repeatGiftTimer.removeMessages(FIRST_GIFT_SEND_FLAG);
+                    repeatGiftTimer.removeMessages(REPEAT_GIFT_SEND_FLAG);
+                    repeatGiftTimer.sendEmptyMessage(REPEAT_GIFT_SEND_FLAG);
+                    repeatTimeLimit=10;
+
+
+
+                }
+            }
+        });
+
     }
+
 }
--- app/src/main/java/com/guaju/adoulive/widget/gift/GiftItem.java
+++ app/src/main/java/com/guaju/adoulive/widget/gift/GiftItem.java
@@ -24,7 +24,11 @@ import com.guaju.adoulive.utils.ImageUtils;
 
 public class GiftItem extends FrameLayout {
 
-    private  int  sendNum=0;
+
+    //发送礼物的数量
+    private int sendNum = 0;
+    //判断是否连发
+    private boolean isRepeat=false;
 
     LayoutInflater inflater;
     private RelativeLayout rl_gift_normal;
@@ -33,7 +37,9 @@ public class GiftItem extends FrameLayout {
     private TextView tv_gift_info;
     private ImageView iv_gift_icon;
     private TextView tv_gift_num;
-    private Animation gift_left_in_anim,gift_icon_left_in_anim,gift_num_scale_anim;
+    private Animation gift_left_in_anim, gift_icon_left_in_anim, gift_num_scale_anim, gift_left_out_anim;
+    private View v;
+    private FrameLayout fl;
 
     public GiftItem(@NonNull Context context) {
         super(context);
@@ -50,12 +56,19 @@ public class GiftItem extends FrameLayout {
     }
 
     private void init() {
-        View v = inflater.inflate(R.layout.item_send_gift, this, true);
+        v = inflater.inflate(R.layout.item_send_gift, this, true);
         {
+            fl = v.findViewById(R.id.fl);
             rl_gift_normal = v.findViewById(R.id.rl_gift_normal);
             iv_sender = v.findViewById(R.id.iv_sender);
             tv_sender_id = v.findViewById(R.id.tv_sender_id);
+            tv_gift_num = v.findViewById(R.id.tv_gift_num);
             tv_gift_info = v.findViewById(R.id.tv_gift_info);
+            iv_gift_icon = v.findViewById(R.id.iv_gift_icon);
+
+            rl_gift_normal.setVisibility(View.INVISIBLE);
+            iv_gift_icon.setVisibility(View.INVISIBLE);
+            tv_gift_num.setVisibility(View.INVISIBLE);
         }
         {
             iv_gift_icon = v.findViewById(R.id.iv_gift_icon);
@@ -71,16 +84,22 @@ public class GiftItem extends FrameLayout {
             gift_left_in_anim = AnimationUtils.loadAnimation(getContext(), R.anim.gift_left_in);
             gift_icon_left_in_anim = AnimationUtils.loadAnimation(getContext(), R.anim.gift_icon_left_in);
             gift_num_scale_anim = AnimationUtils.loadAnimation(getContext(), R.anim.gift_num_scale);
+            gift_left_out_anim = AnimationUtils.loadAnimation(getContext(), R.anim.gift_left_out);
+            //记住动画执行后的效果
+//            gift_left_out_anim.setFillAfter(true);
+//            gift_icon_left_in_anim.setFillAfter(true);
+//            gift_num_scale_anim.setFillAfter(true);
+
             gift_left_in_anim.setAnimationListener(new Animation.AnimationListener() {
                 @Override
                 public void onAnimationStart(Animation animation) {
-                         iv_gift_icon.setVisibility(View.INVISIBLE);
-                         tv_gift_num.setVisibility(View.INVISIBLE);
+                    rl_gift_normal.setVisibility(View.VISIBLE);
                 }
 
                 @Override
                 public void onAnimationEnd(Animation animation) {
-                    iv_gift_icon.setAnimation(gift_icon_left_in_anim);
+                    //给礼物的图标做了动画的设置
+                    iv_gift_icon.startAnimation(gift_icon_left_in_anim);
                 }
 
                 @Override
@@ -88,16 +107,65 @@ public class GiftItem extends FrameLayout {
 
                 }
             });
-                 gift_icon_left_in_anim.setAnimationListener(new Animation.AnimationListener() {
+            gift_icon_left_in_anim.setAnimationListener(new Animation.AnimationListener() {
                 @Override
                 public void onAnimationStart(Animation animation) {
-                       iv_gift_icon.setVisibility(View.VISIBLE);
+                    //把当前的view置为可见
+                    iv_gift_icon.setVisibility(View.VISIBLE);
                 }
 
                 @Override
                 public void onAnimationEnd(Animation animation) {
-                     tv_gift_num.setAnimation(gift_num_scale_anim);
-                     tv_gift_num.setVisibility(View.VISIBLE);
+
+                    //给礼物数做动画
+                    tv_gift_num.startAnimation(gift_num_scale_anim);
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+            //数字的动画
+            gift_num_scale_anim.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    tv_gift_num.setVisibility(View.VISIBLE);
+                    tv_gift_num.setText("x"+sendNum);
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    //所有进入动画执行完成，消失
+                    //判断用户发送礼物数,如果礼物数大于零，并且是连发
+                    if (sendNum>1&&isRepeat){
+                        //再让动画执行
+                        gift_num_scale_anim.start();
+
+                    }else{
+                       //退出动画
+                       rl_gift_normal.startAnimation(gift_left_out_anim);
+                    }
+
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+            gift_left_out_anim.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    iv_gift_icon.setVisibility(View.INVISIBLE);
+                    tv_gift_num.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    rl_gift_normal.setVisibility(View.INVISIBLE);
                 }
 
                 @Override
@@ -117,30 +185,54 @@ public class GiftItem extends FrameLayout {
             } else {
                 ImageUtils.getInstance().loadCircle(info.getAvatar(), iv_sender);
             }
-            if (!TextUtils.isEmpty(info.getAdouID())){
+            if (!TextUtils.isEmpty(info.getAdouID())) {
                 tv_sender_id.setText(info.getAdouID());
             }
-            if (info.getGift()!=null){
-                tv_gift_info.setText("送出了"+info.getGift().getName());
+            if (info.getGift() != null) {
+                tv_gift_info.setText("送出了" + info.getGift().getName());
                 iv_gift_icon.setBackgroundResource(info.getGift().getResId());
             }
-            if (info.getGiftNum()>=1){
-                tv_gift_num.setText("x"+info.getGiftNum());
+            if (info.getGiftNum() >= 1) {
+                tv_gift_num.setText("x" + info.getGiftNum());
             }
 
 
         }
     }
+
     //设置数量
-    public  void  setGiftNum(int num){
-        if (num>1){
-            tv_gift_num.setText("x"+num);
+    public void setGiftNum(int num) {
+        if (num > 1) {
+            tv_gift_num.setText("x" + num);
         }
     }
 
     //让礼物执行动画
-    public  void  startAnimate(){
-        rl_gift_normal.setAnimation(gift_left_in_anim);
+    public void startAnimate() {
+        //给第一个要发生动画的view设置了动画
+        rl_gift_normal.startAnimation(gift_left_in_anim);
+        //礼物数计数为1
+        sendNum=1;
+    }
+
+    //用户连击礼物，增加礼物
+    public void repeatSend(){
+        //让礼物数递增
+        sendNum++;
+        //需要执行数字增加的动画
+        gift_num_scale_anim.start();
+    }
+
+    //用户连击礼物，增加礼物
+    public void repeatSendWithoutAddNum(){
+        //让礼物数递增
+        //退出动画执行
+        rl_gift_normal.setAnimation(gift_num_scale_anim);
+       gift_left_out_anim.start();
+    }
+    //让调用者传入是否连发
+    public void setIsRepeat(boolean isRepeat){
+        this.isRepeat=isRepeat;
     }
 
 
